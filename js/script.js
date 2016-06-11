228e72b4139035cc427e9c5c504e6e78f8186ca8
ZahidDev <zahiddevs@gmail.com>
2016-06-11 14:43:01 -0400

    Major redesign
    Changed color scheme, cleaner landing layout, changed animation easing,
    speed, and exit direction, added "My Work" (WIP), begun work on pages,
    use CDN for most scripts, and cleaned js/css/html files.

--- js/script.js
+++ js/script.js
@@ -1,106 +1,153 @@
 if (jQuery.browser.mobile) {
+    $(document.body).on("touchmove", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+    });
+
     $("#page-bar").hide();
     $(".content-container").hide();
 }
 
 var landAnimation = new TimelineLite();
+var growCurve = BezierEasing(0.4, 0.0, 0.2, 1);
+var easeIn = BezierEasing(0.0, 0.0, 0.2, 1);
+var easeOut = BezierEasing(0.4, 0.0, 1, 1);
 
 function typeAnimate() {
-    $(".about-peek").typed({
-        strings: ["Undergraduate student at York University studying Machine Learning and Artificial Intelligence. <br /> Android development hobbyist, concept developer for Deep Learning Neural Networks, and UI designer."],
-        typeSpeed: -10,
-        callback: function() {
-            landAnimation.to("#i-am", 0.4, {
-                scaleX: 1,
-                scaleY: 1,
-                ease: Sine.easeOut,
-                onComplete: function() {
-                    $(".adj-list").typed({
-                        strings: ["INNOVATOR", "LEADER", "DEVELOPER", "TEAM PLAYER", "LEARNER", "THINKER"],
-                        showCursor: false,
-                        typeSpeed: 75,
-                        startDelay: 300,
-                        backDelay: 2500,
-                        loop: true
-                    });
-                }
+    landAnimation.to("#i-am", 0.4, {
+        scaleX: 1,
+        scaleY: 1,
+        ease: new Ease(growCurve),
+        onComplete: function () {
+            $(".adj-list").typed({
+                strings: ["INNOVATOR", "LEADER", "DEVELOPER", "TEAM PLAYER", "LEARNER",
+                    "THINKER"
+                ],
+                showCursor: false,
+                typeSpeed: 75,
+                startDelay: 300,
+                backDelay: 2500,
+                loop: true
             });
         }
     });
 }
 
-$(document).ready(function() {
 
-    landAnimation.to("#landing", 0.8, {
-            css: {
-                border: "0.75em solid white"
-            },
-            ease: Sine.easeOut
-        })
-        .to("#logo", 0.8, {
-            scaleX: 1,
-            scaleY: 1,
-            rotation: 360,
-            ease: Sine.easeOut
-        }, "+=0.25")
-        .to("#logo", 0.5, {
-            className: "z-depth-1",
-            ease: Sine.easeOut
-        })
-        .to(".text-logo", 0.5, {
-            scaleX: 1,
-            scaleY: 1,
-            ease: Sine.easeOut
-        });
+$(document).ready(function () {
+
+    landAnimation.to("#logo", 0.8, {
+        css: {
+            opacity: "0"
+        },
+        ease: new Ease(growCurve)
+    }, "+=0.75")
+
+    .to(".bg-layer", 0.8, {
+        css: {
+            border: "0.75em solid white"
+        },
+        ease: new Ease(growCurve)
+    })
+
+    .to(".text-logo", 0.5, {
+        scaleX: 1,
+        scaleY: 1,
+        ease: new Ease(growCurve)
+    }, "+=0.25");
 
     if (!jQuery.browser.mobile) {
         landAnimation.to("#page-bar", 0.5, {
             css: {
-                marginTop: "6.5em"
+                marginTop: "11em"
             },
-            ease: Sine.easeOut
-        });
+            ease: new Ease(easeIn)
+        }, "-=0.5");
     }
 
-    landAnimation.to("#peek", 0.4, {
-        scaleX: 1,
-        scaleY: 1,
-        ease: Sine.easeOut,
-        onComplete: typeAnimate
-    }, "+= 0.3");
+    typeAnimate();
 });
+
 
 if (!jQuery.browser.mobile) {
     var navAnimation = new TimelineLite();
+    var pageAnimation = new TimelineLite();
 
-    function buttonDelay(URL) {
-        if (URL == "#about-me") {
-            $(".content").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi facilisis est vitae ipsum mattis blandit. Integer quis elit lectus. Cras ac porta nibh. Vestibulum accumsan tincidunt semper. Morbi aliquet sapien a placerat fringilla. Aliquam posuere condimentum orci sagittis suscipit. Nam pharetra vestibulum rutrum. Aenean quis velit laoreet, porttitor tortor eget, pharetra leo. Vestibulum enim leo, elementum sit amet bibendum vehicula, finibus non massa. Nullam sed porta orci. Duis a tortor et dolor interdum vulputate. Quisque ultrices commodo metus, at interdum velit luctus et. Curabitur a venenatis enim. Sed venenatis nisi nec massa hendrerit, quis convallis felis dapibus.");
-        } else if (URL == "#contact-me") {
-            $(".content").text(" A bridge approves a sunrise noble. My bush bulletin crawls. Opposite the neighbor barks a spiral packet. Without a rear talks a rhetorical sufferer. The cheerful anagram fulfills the subway. A magazine chooses the stuff. The commentary suspects outside the shout! An adverse jelly results outside the native politician. How will the twist choose? Below an artist thinks the message.");
+    function buttonDelay(button, page) {
+
+        if ($('#page-bar div').hasClass('button-clicked z-depth-1')) {
+            $('#page-bar div').removeClass('button-clicked z-depth-1');
+            $(button).toggleClass('button-clicked z-depth-1');
+        } else {
+            $(button).addClass('button-clicked z-depth-1');
         }
 
-        setTimeout(function() {
-            navAnimation.to(".content-container", 0.5, {
-                css: {
-                    top: "2em"
-                },
-                ease: Power2.easeOut
-            });
-        }, 500);
+        if ($('.content-container div').hasClass('visible')) {
+            $('.content-container div').removeClass('visible');
+            $(page).toggleClass('visible');
+        } else {
+            $(page).addClass('visible');
+        }
+
+        setTimeout(function () {
+            if (!$(".content-container").hasClass('active')) {
+                navAnimation.to(".content-container", 0.3, {
+                    z: 0.01,
+                    rotationZ: 0.01,
+                    css: {
+                        top: "2em"
+                    },
+                    ease: new Ease(easeOut),
+                    force3d: true,
+                    onComplete: function () {
+                        $(".content-container").addClass("active");
+                    }
+                });
+            }
+        }, 250);
     }
 
-    $(document).mouseup(function(e) {
+    function closePanel() {
+        $("#page-bar div").removeClass("button-clicked z-depth-1");
+        navAnimation.to(".content-container", 0.5, {
+            css: {
+                left: "150%"
+            },
+            z: 0.1,
+            rotationZ: 0.01,
+            ease: new Ease(easeIn),
+            force3d: true,
+            onComplete: function () {
+                $(".content-container").removeClass("active");
+            }
+        })
+
+        .to(".content-container", 0.01, {
+            css: {
+                top: "51.5em"
+            }
+        })
+
+        .to(".content-container", 0.01, {
+            css: {
+                left: "50%",
+            },
+            onComplete: function () {
+                navAnimation.clear();
+            }
+        });
+    }
+
+    $(document).mouseup(function (e) {
         var container = $(".content-container");
         var pageButton = $(".page-button");
+        $("#close").on("click", function () {
+            closePanel();
+        });
 
-        if ((!container.is(e.target) && container.has(e.target).length === 0) && (!pageButton.is(e.target) && pageButton.has(e.target).length === 0)) {
-            navAnimation.to(container, 0.5, {
-                css: {
-                    top: "50em"
-                },
-                ease: Power2.easeIn
-            });
+        if ((!container.is(e.target) && container.has(e.target).length === 0) && (!pageButton.is(e.target) &&
+                pageButton.has(e.target).length === 0) && (container.hasClass("active"))) {
+            closePanel();
         }
     });
 }
