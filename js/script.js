8417e46c8326ea8bf36a6baba1c6539d07072c89
ZahidDev <zahiddevs@gmail.com>
2016-06-12 23:41:16 -0400

    testing em

--- js/script.js
+++ js/script.js
@@ -85,7 +85,7 @@ if (!jQuery.browser.mobile) {
                     z: 0.01,
                     rotationZ: 0.01,
                     css: {
-                        top: "5%"
+                        top: "5em"
                     },
                     ease: new Ease(easeOut),
                     force3d: true,
@@ -114,7 +114,7 @@ if (!jQuery.browser.mobile) {
 
         .to(".content-container", 0.01, {
             css: {
-                top: "150%"
+                top: "175em"
             }
         })
 
